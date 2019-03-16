a3bc0081cc75f8ca862203e56452cbc68fa7548a
perfecttutorial <48180399+perfecttutorial@users.noreply.github.com>
2019-03-16 15:53:23 +0530

    Add files via upload

---
+++ app/menu-tabs/menu-tabs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'menu-tabs',
+  templateUrl: './menu-tabs.component.html',
+  styleUrls: ['./menu-tabs.component.css']
+})
+export class MenuTabsComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
